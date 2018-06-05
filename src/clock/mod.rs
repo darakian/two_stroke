1f91fd41fe5ac172af116871d2a8618669a5ed68
Jon Moroney <darakian@gmail.com>
2018-06-05 13:02:27 -1000

    Remove comments

--- src/clock/mod.rs
+++ src/clock/mod.rs
@@ -1,26 +1,3 @@
-// use std::thread;
-// use std::sync::mpsc::channel;
-//
-// fn main() {
-//     let start_time = Instant::now();
-//     let (tx, rx) = channel();
-//     // Threaded one millisecond timer
-//     thread::spawn(move || {
-//     loop {
-//         thread::sleep(Duration::new(0, 16666666));
-//         tx.send("tick").unwrap();
-//     }
-//     });
-//
-//
-//
-//
-//     for entry in rx.iter() {
-//         if start_time.elapsed() >= Duration::from_secs(1) {break}
-//     }
-//
-// }
-
 pub mod clock {
     use std::time::{Duration, Instant};
     use std::thread;
@@ -47,7 +24,7 @@ pub mod clock {
                 self.sender.send(
                     omnibus::Message::new_tick("all", self.message_id, Instant::now()))
                 .unwrap();
-                
+
                 thread::sleep(self.tick_step);
             }
         }
