e7f804fd5ce569e161b4afcdb0af102dabf32ef3
Jon Moroney <darakian@gmail.com>
2018-06-06 19:03:06 -1000

    Working clock

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use std::time::Duration;
 mod bad_rng;
 use bad_rng::lfsr_rng;
 use std::sync::Arc;
+use std::thread;
 
 
 fn main() {
@@ -13,6 +14,9 @@ fn main() {
     let count = clock_module::clock::TheCount::new(Duration::new(1, 0), 10, mb.join(10).unwrap());
     mb.publish(Arc::new(omnibus::Message::new_sub("bus", 2, "test")));
     bad_rand.step();
+    let h1 = thread::spawn(move || {
+        count.run();
+    });
     //println!("Hello, world! {}", bad_rand.sample());
     mb.do_messaging();
 }
