90c92c49920337d5570ccdbbf99ce123a971ffca
Jon Moroney <darakian@gmail.com>
2018-06-06 17:56:37 -1000

    Rename clock

--- src/clock_module/mod.rs
+++ src/clock_module/mod.rs
@@ -3,7 +3,6 @@ pub mod clock {
     use std::thread;
     use std::sync::Arc;
     extern crate crossbeam_channel;
-    use self::crossbeam_channel::unbounded;
     use messaging_module::omnibus;
     use messaging_module::omnibus::Message;
 
@@ -12,12 +11,13 @@ pub mod clock {
         start_time: Instant,
         tick_step: Duration,
         message_id: u64,
-        sender: crossbeam_channel::Sender<Arc<Message>>
+        sender: crossbeam_channel::Sender<Arc<Message>>,
+        reciever: crossbeam_channel::Receiver<Arc<Message>>
     }
 
     impl TheCount{
-        pub fn new(step: Duration, id: u64, channel: crossbeam_channel::Sender<Arc<Message>>) -> Self{
-            TheCount{start_time: Instant::now(), tick_step: step, message_id: id,sender: channel}
+        pub fn new(step: Duration, id: u64, channels: (crossbeam_channel::Sender<Arc<Message>>, crossbeam_channel::Receiver<Arc<Message>>)) -> Self{
+            TheCount{start_time: Instant::now(), tick_step: step, message_id: id, sender: channels.0, reciever: channels.1}
         }
 
         pub fn run(&self){
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod messaging_module;
 use messaging_module::omnibus;
-mod clock;
+mod clock_module;
+use std::time::Duration;
 mod bad_rng;
 use bad_rng::lfsr_rng;
 use std::sync::Arc;
@@ -9,6 +10,7 @@ use std::sync::Arc;
 fn main() {
     let mut bad_rand = lfsr_rng::StatefulLfsr::new(11);
     let mut mb = omnibus::Omnibus::new("bus");
+    let count = clock_module::clock::TheCount::new(Duration::new(1, 0), 10, mb.join(10).unwrap());
     mb.publish(Arc::new(omnibus::Message::new_sub("bus", 2, "test")));
     bad_rand.step();
     //println!("Hello, world! {}", bad_rand.sample());
