038b088fbdaff0f50041c669f749c294223978cd
Jon Moroney <darakian@gmail.com>
2018-06-17 11:49:25 -1000

    Add channel to input

--- src/input_module/mod.rs
+++ src/input_module/mod.rs
@@ -3,26 +3,33 @@ pub mod input_scanner {
     use self::sdl2::keyboard::Keycode;
     use self::sdl2::keyboard::Scancode;
     use std::collections::HashSet;
+    use std::sync::Arc;
+    extern crate crossbeam_channel;
+    use messaging_module::omnibus;
+    use messaging_module::omnibus::Message;
 
     pub struct Inputmanager{
         last_key_state: HashSet<Scancode>,
         sld_context: sdl2::Sdl,
-        event_pump: sdl2::EventPump
+        event_pump: sdl2::EventPump,
+        message_id: u64,
+        sender: crossbeam_channel::Sender<Arc<Message>>,
+        reciever: crossbeam_channel::Receiver<Arc<Message>>
     }
 
     impl Inputmanager{
-        pub fn new() -> Inputmanager{
+        pub fn new(id: u64, channels: (crossbeam_channel::Sender<Arc<Message>>, crossbeam_channel::Receiver<Arc<Message>>)) -> Inputmanager{
             let context = sdl2::init().unwrap();
             let events = context.event_pump().unwrap();
-            Inputmanager{last_key_state: HashSet::new(), sld_context: context, event_pump: events}
+            Inputmanager{last_key_state: HashSet::new(), sld_context: context, event_pump: events, message_id: id, sender: channels.0, reciever: channels.1}
         }
 
-        fn pressed_scancode_set(e: &sdl2::EventPump) -> HashSet<Scancode> {
-            e.keyboard_state().pressed_scancodes().collect()
+        fn pressed_scancode_set(&self) -> HashSet<Scancode> {
+            self.event_pump.keyboard_state().pressed_scancodes().collect()
         }
 
-        fn pressed_keycode_set(e: &sdl2::EventPump) -> HashSet<Keycode> {
-            e.keyboard_state().pressed_scancodes()
+        fn pressed_keycode_set(&self) -> HashSet<Keycode> {
+            self.event_pump.keyboard_state().pressed_scancodes()
                 .filter_map(Keycode::from_scancode)
                 .collect()
         }
@@ -30,6 +37,12 @@ pub mod input_scanner {
         fn newly_pressed(old: &HashSet<Scancode>, new: &HashSet<Scancode>) -> HashSet<Scancode> {
             new - old
             // sugar for: new.difference(old).collect()
+        }
+
+        pub fn print_scancodes(&self){
+            for code in Inputmanager::pressed_keycode_set(self){
+                println!("{:?}", code);
+            }
         }
 
     }
--- src/main.rs
+++ src/main.rs
@@ -16,13 +16,14 @@ fn main() {
 
     let count = clock_module::clock::TheCount::new(Duration::new(1, 0), 10, mb.join(10).unwrap());
     let mut bad_rand = bad_rng::StatefulLfsr::new(11, 11, mb.join(10).unwrap());
+    let my_input = input_module::input_scanner::Inputmanager::new(12, mb.join(10).unwrap());
+
 
     mb.publish(Arc::new(omnibus::Message::new_sub("bus", 2, "test")));
     bad_rand.step();
     let h1 = thread::spawn(move || {
         count.run();
     });
-    let my_input = input_module::input_scanner::Inputmanager::new();
     //println!("Hello, world! {}", bad_rand.sample());
     mb.do_messaging();
     loop {
